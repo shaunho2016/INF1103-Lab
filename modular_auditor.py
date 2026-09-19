e69f3f44cfc2515d5fafa643ac4ba55e045161a0
Shaun Ho <2603202@sit.singaporetech.edu.sg>
2026-09-19 16:23:05 +0800

    Made inventory and failed entries global var and created loop for report

--- modular_auditor.py
+++ modular_auditor.py
@@ -2,6 +2,7 @@ inventory = 0
 failed_entries = 0
 
 def get_valid_input():
+    global failed_entries
     stock = input("Enter stock quantity (or type 'quit' to stop): ")
     # Check if quit
     if stock.lower() == "quit":
@@ -14,12 +15,17 @@ def get_valid_input():
             return(stock)
         else:
             print("Error: Negative stock values are not allowed.")
+            failed_entries += 1
+            return(False)
     else:
         print("Error: Invalid input. Please enter a number.")
+        failed_entries += 1
+        return(False)
         
 def process_delivery(current_total,new_value):
-    new_total = current_total + new_value
-    return(new_total)
+    global inventory
+    inventory = current_total + new_value
+    return(inventory)
 
 def calculate_tax(amount):
     tax = amount * 0.1
@@ -30,3 +36,12 @@ def generate_report(total_units,failed_attempts):
     print("Total Units Processed:", total_units)
     print("Number of Failed/Rejected Entries:", failed_attempts)
 
+while True:
+    inv = get_valid_input()
+    if inv == "quit":
+        break
+
+    print(f'Current inventory: {process_delivery(inventory,inv)}')
+    print(f'Current tax: {calculate_tax(inventory)}')
+
+print(generate_report(inventory,failed_entries))
